0b1b48acd7c0b7d519da14c4ce8629500ace0cca
akpandawep <154840412+akpandawep@users.noreply.github.com>
2024-01-09 21:18:26 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -90,7 +90,7 @@ async def start(client, message):
                 )
             filetype = msg.media
             file = getattr(msg, filetype.value)
-            title = '[Joine For More](https://t.me/Pandawep)  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
+            title = '@PandaWep  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
             size=get_size(file.file_size)
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
