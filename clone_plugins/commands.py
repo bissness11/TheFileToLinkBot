8a9c8dc6a045faa227aeeb394f549099fd98a292
akpandawep <154840412+akpandawep@users.noreply.github.com>
2024-01-14 00:38:30 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -106,7 +106,7 @@ async def start(client, message):
             return
         except:
             pass
-        return await message.reply('No such file exist.')
+        return await message.reply('Bot Creator - @PandaWep')
     files = files_[0]
     title = files.file_name
     size=get_size(files.file_size)
