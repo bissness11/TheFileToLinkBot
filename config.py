ee42158ecb744ba422c971da63e00a1a1a1bc258
bissness11 <154408458+bissness11@users.noreply.github.com>
2024-08-17 16:54:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -85,9 +85,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://pandawepnew-pandawepnew.koyeb.app/"
+        URL = "https://exceptional-brittaney-bissness-13593939.koyeb.app/"
     else:
-        URL = "https://pandawepnew-pandawepnew.koyeb.app/"
+        URL = "https://exceptional-brittaney-bissness-13593939.koyeb.app/"
 
 
 
